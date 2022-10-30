8bfa5925d861392ef8d845dfa1ee9b4161d36152
Shadow-Nino <108553512+Shadow-Miku@users.noreply.github.com>
2022-10-29 23:32:16 -0500

    Experimento de las noticias
    Probando que y que no se puede hacer

--- Repaso/resources/views/principal.blade.php
+++ Repaso/resources/views/principal.blade.php
@@ -186,62 +186,62 @@ div, h2, p, figure {
 
         <main class="main columns">
             <section class="column main-column">
-              <a class="article first-article" href="#">
+              <a class="article first-article" href="https://www.unicef.org/es/end-violence/concurso-comics-superheroe-escuela">
                 <figure class="article-image is-4by3">
-                  <img src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/1366/posts/26611/attachment/image-01.png" alt="">
+                  <img src="https://www.unicef.org/sites/default/files/styles/hero_desktop/public/HERO%20IMAGE%20COMIC%20BOOK.jpg?itok=nMQDAv9z" alt="">
                 </figure>
                 <div class="article-body">
                   <h2 class="article-title">
-                    Hello World
+                    Ganador del concurso de comics UNICEF
                   </h2>
                   <p class="article-content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros.
                   </p>
                   <footer class="article-info">
-                    <span>By Joe Smith</span>
-                    <span>42 comments</span>
+                    <span>Por John Blaskowithz</span>
+                    <span>1814 comments</span>
                   </footer>
                 </div>
               </a>
           
               <div class="columns">
                 <div class="column nested-column">
-                  <a class="article" href="#">
+                  <a class="article" href="https://aweita.larepublica.pe/anime-manga-y-comics/2021/08/24/kaijuu-no8-oshi-ko-se-consagran-como-ganadores-next-manga-awards-2021-7052">
                     <figure class="article-image is-16by9">
-                      <img src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/1366/posts/26611/attachment/image-02.png" alt="">
+                      <img src="https://imgmedia.aweita.pe/640x345/aweita/original/2021/08/24/61250d01fa3072673851a06d.webp" alt="">
                     </figure>
                     <div class="article-body">
                       <h2 class="article-title">
-                        Hello World
+                        Especiales del mundo de anime
                       </h2>
                       <p class="article-content">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
                       </p>
                       <footer class="article-info">
-                        <span>By Joe Smith</span>
-                        <span>42 comments</span>
+                        <span>By Aldo Rojas</span>
+                        <span>785 comments</span>
                       </footer>
                     </div>
                   </a>
                 </div>
           
                 <div class="column">
-                  <a class="article" href="#">
+                  <a class="article" href="https://www.emaze.com/@ACQITRIZ">
                     <figure class="article-image is-16by9">
-                      <img src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/1366/posts/26611/attachment/image-03.png" alt="">
+                      <img src="https://userscontent2.emaze.com/images/34c08dde-cf17-4cf9-a162-b52cd99fee68/b7184a88c93e1bf2aeddfe37ca91ecf3.jpg" alt="">
                     </figure>
                     <div class="article-body">
                       <h2 class="article-title">
-                        Hello World
+                        Como los libros han ayudado a la libertad de expresión 
                       </h2>
                       <p class="article-content">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                       </p>
                       <footer class="article-info">
-                        <span>By Joe Smith</span>
-                        <span>42 comments</span>
+                        <span>By Alejandro Vargas</span>
+                        <span>379 comments</span>
                       </footer>
                     </div>
                   </a>
@@ -278,13 +278,13 @@ div, h2, p, figure {
             </section>
           
             <section class="column">
-              <a class="article" href="#">
+              <a class="article" href="https://es.resonancescience.org/blog/masa-negativa-creada-en-la-universidad-estatal-de-washington">
                 <figure class="article-image is-3by2">
-                  <img src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/1366/posts/26611/attachment/image-04.png" alt="">
+                  <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/blogs/26406/images/Kluk7dBzQ1eMtXl5QvK2_9.jpeg" alt="">
                 </figure>
                 <div class="article-body">
                   <h2 class="article-title">
-                    Hello World
+                    La masa negativa
                   </h2>
                   <p class="article-content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
